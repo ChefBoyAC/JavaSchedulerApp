d587b46d5f4ac959ce0eb49b62a8a30114ff8e2a
Aaron Channer <aachanner1@gmail.com>
2024-01-24 16:45:07 -0500

    first commit

--- app/src/main/java/com/example/javaschedulerapp/view/UserAdapter.java
+++ app/src/main/java/com/example/javaschedulerapp/view/UserAdapter.java
@@ -1,11 +1,15 @@
 package com.example.javaschedulerapp.view;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -31,12 +35,16 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
         TextView userLocation;
 
+        //get the linear layout of the row
+        LinearLayout listLayoutRow;
+
         public UserViewHolder(View v) {
             super(v);
             name = v.findViewById(R.id.mTitle);
             userSchedule = v.findViewById(R.id.subTitle);
             userInstructor = v.findViewById(R.id.anotherSubTitle);
             userLocation = v.findViewById(R.id.anotherAnotherSubTitle);
+            listLayoutRow = v.findViewById(R.id.listLayout);
         }
     }
 
@@ -54,10 +62,35 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         holder.userSchedule.setText(newList.getUserSchedule());
         holder.userInstructor.setText(newList.getUserInstructor());
         holder.userLocation.setText(newList.getUserLocation());
+
+        //this is where we update the code
+        holder.listLayoutRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //make a new dialog with the parameters of the courses
+                /**AlertDialog dialog = new AlertDialog.Builder(context);
+                dialog.setContentView(R.layout.add_item);
+
+                EditText courseName = dialog.findViewById(R.id.courseName);
+                EditText dateAndTimeOfClass = dialog.findViewById(R.id.dateAndTimeOfClass);
+                EditText instructor = dialog.findViewById(R.id.instructor);
+                EditText location = dialog.findViewById(R.id.location);
+                //include the cancel and update button
+
+                //change the current code in the system
+
+                //show the dialog to the screen
+                dialog.show();
+                 **/
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
         return userList.size();
     }
+
+
 }
